b738c0d8aaee7ec49643d50e72d711a514883d19
Tony071101 <tinpigboyapple07112001@gmail.com>
2023-10-02 15:19:01 +0700

    feat: adding game build

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using TMPro;
-using UnityEditor.U2D;
 using UnityEngine;
 
 public class Player : BaseCharacter
